12ea5ca819a900ba2e67bec3cc727bcb56d9ca6e
Scar Wu <xneriscool@gmail.com>
2017-01-25 18:52:13 +0800

    update converter

--- src/convertForWord2vec.php
+++ src/convertForWord2vec.php
@@ -3,10 +3,28 @@
 
 function utf8($num)
 {
-    if($num<=0x7F)       return chr($num);
-    if($num<=0x7FF)      return chr(($num>>6)+192).chr(($num&63)+128);
-    if($num<=0xFFFF)     return chr(($num>>12)+224).chr((($num>>6)&63)+128).chr(($num&63)+128);
-    if($num<=0x1FFFFF)   return chr(($num>>18)+240).chr((($num>>12)&63)+128).chr((($num>>6)&63)+128).chr(($num&63)+128);
+    if ($num <= 0x7F) {
+        return chr($num);
+    }
+
+    if ($num <= 0x7FF) {
+        return chr(($num >> 6) + 192)
+            . chr(($num & 63) + 128);
+    }
+
+    if ($num <= 0xFFFF) {
+        return chr(($num >> 12) + 224)
+            . chr((($num >> 6) & 63) + 128)
+            . chr(($num & 63) + 128);
+    }
+
+    if ($num <= 0x1FFFFF) {
+        return chr(($num >> 18) + 240)
+            . chr((($num >> 12) & 63) + 128)
+            . chr((($num >> 6) & 63) + 128)
+            . chr(($num & 63) + 128);
+    }
+
     return '';
 }
 
@@ -77,7 +95,7 @@ foreach ($inputData as $data) {
                 }
 
                 if (isset($definition['quote'])) {
-                    $text[] = $definition['quote'];
+                    $text[] = implode(' ', $definition['quote']);
                 }
             }
         }
